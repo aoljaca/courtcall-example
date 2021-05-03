2fd0ff7076205140ae073a63d97e46d76c801f70
Michael Sila <silaarasm@gmail.com>
2021-05-03 10:08:29 -0600

    starting with vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import VueSweetalert2 from "vue-sweetalert2";
-import "sweetalert2/dist/sweetalert2.min.css";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import buildDependencyContainer from "./inversify/container";
 import 'reflect-metadata';
 Vue.config.productionTip = false;
@@ -16,4 +16,4 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
-Vue.use(VueSweetalert2);
+Vue.use(Toast,{});
--- src/services/toast.ts
+++ src/services/toast.ts
@@ -1,23 +1,15 @@
-import { injectable } from 'inversify-props';
-import Vue from 'vue';
+import { injectable } from "inversify-props";
+import Vue from "vue";
 
 export interface ToastService {
-    standardToast : () => void,
-
+  standardToast: () => void;
 }
 
 @injectable()
 export class ToastServiceImpl {
-
-    public standardToast() {
-        Vue.swal.fire({
-            title: 'Example Alert',
-            timer: 3000,
-            text: 'This message will be dismissed in 3 seconds',
-            position: 'top-end',
-            toast: true,
-            timerProgressBar: true,
-            showConfirmButton: false
-        })
-    }
+  public standardToast() {
+    Vue.$toast('Example Toast',{
+      timeout: 3000
+    })
+  }
 }
