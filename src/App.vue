9c2e0bcec57a6af87149b23e717cdfd31a21c7cd
Michael Sila <silaarasm@gmail.com>
2021-05-03 10:17:34 -0600

    getting navigation drawer working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
     <v-app id="app">
-      <v-system-bar color="blue darken-3"></v-system-bar>
-      <router-link to="/">Home</router-link> 
-      <router-link to="/about">About</router-link>
-    <router-view />
+      <router-view />
     </v-app>
 
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
+    <v-app-bar color="blue accent-3">
+      <v-app-bar-nav-icon color="white" @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary></v-navigation-drawer>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
@@ -14,5 +18,17 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+  public drawer = false;
+  public group = null;
+  toggleDrawer() {
+    this.drawer = !this.drawer;
+  }
+
+  @Watch('group')
+  onPropertyChanged(oldVal: string, newVal: string) {
+    this.drawer = false
+  }
+}
 </script>
