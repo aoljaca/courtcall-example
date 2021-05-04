bb99d4440a27168698e64b4d19212a96c64124dd
Michael Sila <silaarasm@gmail.com>
2021-05-04 12:49:59 -0600

    stubbing out methods

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,6 @@
 </template>
 
 <style lang="scss">
-.d-flex {
-  display: flex;
-}
-.align-items-center {
-  align-items: center;
-}
-.justify-content-center {
-  justify-content: center;
-}
-.justify-items-center {
-  justify-items: center;
-}
-.w-50 {
-  width: 50%;
-}
+@import './styles/utilities.scss';
+@import './styles/variables.scss';
 </style>
