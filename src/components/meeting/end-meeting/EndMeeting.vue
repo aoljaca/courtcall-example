3d1a042aa7c7a88fb10b53663d472354746fbd65
Michael Sila <silaarasm@gmail.com>
2021-05-05 12:16:19 -0600

    meeting page

---
+++ src/components/meeting/end-meeting/EndMeeting.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="endMeeting">
+        <v-container class="fill d-flex align-content-center">
+            <v-row class="justify-center">
+                <v-col align-self="center" cols="6">
+                    <v-card class="background-gray"></v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';;
+@Component 
+export default class EndMeeting extends Vue {
+
+}
+</script>
+<style lang="scss" scoped>
+
+.fill {
+    height: 100vh;
+}
+.background-gray {
+    background-color: lightgray;
+}
+</style>
