69272781517e67f7e1e20c368fddb347d38e926c
Michael Sila <silaarasm@gmail.com>
2021-05-08 15:21:56 -0600

    Change backgrounds to not be open

--- src/components/meeting/av-options/VideoPreview.vue
+++ src/components/meeting/av-options/VideoPreview.vue
@@ -108,7 +108,7 @@ export default class VideoPreview extends Vue {
     display: "none",
   };
   videoVisible = false;
-  openBackground = true;
+  openBackground = false;
   readonly noBackgroundOption: BackgroundOption = {
       type: 'none'
   }
