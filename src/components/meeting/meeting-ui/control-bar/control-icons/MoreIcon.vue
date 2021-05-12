a17e50d86b92439a7f54aca430efc78341de09c7
Michael Sila <silaarasm@gmail.com>
2021-05-12 12:42:13 -0600

    changing icon value to false

--- src/components/meeting/meeting-ui/control-bar/control-icons/MoreIcon.vue
+++ src/components/meeting/meeting-ui/control-bar/control-icons/MoreIcon.vue
@@ -165,7 +165,7 @@ export default class MoreIcon extends Vue {
     avSetup: false,
     notificationPreferences: false,
     transcription: false,
-    publicStreaming: true,
+    publicStreaming: false,
   };
 }
 </script>
