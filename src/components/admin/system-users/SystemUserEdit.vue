a1d917e53ae4bfa132bc2b05972b02a9e845ecde
paulthomasjerome <contactpauljerome@gmail.com>
2021-05-13 23:39:03 -0700

    Fix row height issue

--- src/components/admin/system-users/SystemUserEdit.vue
+++ src/components/admin/system-users/SystemUserEdit.vue
@@ -5,8 +5,8 @@
         <h2>{{ $t("admin.systemUser.header") }}</h2>
       </v-col>
     </v-row>
-    <v-form class="ml-16">
-      <v-row class="mt-16">
+    <v-form class="ml-16 form">
+      <v-row class="mt-16 mb-4">
         <v-col cols="6">
           <h1>Frank Voci</h1>
         </v-col>
@@ -21,53 +21,53 @@
           </v-row>
         </v-col>
       </v-row>
-      <v-row>
-        <v-col class="mt-6" cols="2">
+      <v-row dense>
+        <v-col cols="2">
           <label>{{ $t("admin.systemUser.email") }}</label>
         </v-col>
         <v-col cols="3">
-          <v-text-field>
+          <v-text-field dense>
           </v-text-field>
         </v-col>
       </v-row>
-      <v-row>
-        <v-col class="mt-6" cols="2">
+      <v-row dense>
+        <v-col cols="2">
           <label>{{ $t("admin.systemUser.phone") }}</label>
         </v-col>
         <v-col cols="3">
-          <v-text-field>
+          <v-text-field dense>
           </v-text-field>
         </v-col>
       </v-row>
-      <v-row>
+      <v-row class="mt-8 mb-4" dense>
         <v-col>
           <h2>{{ $t("admin.systemUser.changePassword") }}</h2>
         </v-col>
       </v-row>
-      <v-row>
-        <v-col class="mt-6" cols="2">
+      <v-row dense>
+        <v-col cols="2">
           <label>{{ $t("admin.systemUser.password") }}</label>
         </v-col>
         <v-col cols="3">
-          <v-text-field>
+          <v-text-field dense>
           </v-text-field>
         </v-col>
       </v-row>
-      <v-row>
-        <v-col class="mt-6" cols="2">
+      <v-row dense>
+        <v-col cols="2">
           <label>{{ $t("admin.systemUser.changePassword") }}</label>
         </v-col>
         <v-col cols="3">
-          <v-text-field>
+          <v-text-field dense>
           </v-text-field>
         </v-col>
       </v-row>
-      <v-row>
-        <v-col class="mt-6" cols="2">
+      <v-row dense>
+        <v-col cols="2">
           <label>{{ $t("admin.systemUser.newPassword") }}</label>
         </v-col>
         <v-col cols="3">
-          <v-text-field>
+          <v-text-field dense>
           </v-text-field>
         </v-col>
       </v-row>
@@ -92,13 +92,5 @@ export default class SystemUserEdit extends Vue {}
 <style lang="scss" scoped>
 .end-row {
   border-bottom: 1px solid gray;
-}
-
-.justify-content-right {
-  justify-content: right;
-}
-
-.button-margin {
-  margin: 1rem;
 }
 </style>
