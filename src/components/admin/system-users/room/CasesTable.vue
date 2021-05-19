45491233a779971bab85f38a5c02f44d1962d921
Paul Jerome <contactpauljerome@gmail.com>
2021-05-19 09:00:24 -0700

    Incorporate basic data tables for CasesTable.vue and ParticipantsTable.vue

--- src/components/admin/system-users/room/CasesTable.vue
+++ src/components/admin/system-users/room/CasesTable.vue
@@ -1,14 +1,96 @@
 <template>
-  <div>
-  </div>     
+  <div class="rooms-table">
+    <v-container fluid>
+      <v-row class="mb-4">
+        <v-col>
+          <h2>Cases</h2>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn
+          color="grey darken-4 rounded-0 white--text"
+          depressed
+          >
+            ADD
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-data-table
+            :headers="headers"
+            :items="tableData"
+            :items-per-page="20"
+            class="elevation-1"
+          >
+            <template v-slot:item.active="{ item }">
+              <v-icon v-if="item.active" color="green">mdi-circle</v-icon>
+              <v-icon v-else color="gray">mdi-circle</v-icon>
+            </template>
+
+            <template v-slot:item.streaming="{ item }">
+              <v-icon v-if="item.streaming" color="black"
+                >mdi-access-point</v-icon
+              >
+            </template>
+
+            <template v-slot:item.recording="{ item }">
+              <v-icon v-if="item.recording" color="red">mdi-circle</v-icon>
+            </template>
+
+            <template v-slot:item.more="{ item }">
+              <v-icon :color="item.more">mdi-dots-horizontal</v-icon>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import "reflect-metadata";
 @Component
-export default class CasesTable extends Vue {}
+export default class CasesTable extends Vue {
+  data() {
+    return {
+      headers: [
+        { text: "Active", value: "active", align: "center" },
+        { text: "Case Number", value: "caseNumber", align: "center" },
+        { text: "Case Name", value: "caseName", align: "center" },
+        { text: "Scheduled Participants", value: "scheduledParticipants", align: "center" },
+        { text: " ", value: "more", align: "center" },
+      ],
+      tableData: [
+        {
+          active: true,
+          caseNumber: "1234567890",
+          caseName: "Boris v. Natasha",
+          scheduledParticipants: "Olivia Coleman, Viola Davis, Tom Hanks + 5 More",
+          more: "",
+        },
+        {
+          active: false,
+          caseNumber: "2345678901",
+          caseName: "Carrell Arbitration",
+          scheduledParticipants: "Danny Trejo, Steve Carrell, Viola Davis + 3 More",
+          more: "",
+        },
+        {
+          active: false,
+          caseNumber: "3456789012",
+          caseName: "Boris v. Natasha",
+          scheduledParticipants: "Boris Smith, Natasha Smith",
+          more: "",
+        },
+      ],
+    };
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
+
+
+
--- src/components/admin/system-users/room/ParticipantsTable.vue
+++ src/components/admin/system-users/room/ParticipantsTable.vue
@@ -1,14 +1,97 @@
 <template>
-  <div>
-  </div>     
+  <div class="rooms-table">
+    <v-container fluid>
+      <v-row class="mb-4">
+        <v-col>
+          <h2>Participants</h2>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn
+          color="grey darken-4 rounded-0 white--text"
+          depressed
+          >
+            ADD
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-data-table
+            :headers="headers"
+            :items="tableData"
+            :items-per-page="20"
+            class="elevation-1"
+          >
+            <template v-slot:item.active="{ item }">
+              <v-icon v-if="item.active" color="green">mdi-circle</v-icon>
+              <v-icon v-else color="gray">mdi-circle</v-icon>
+            </template>
+
+            <template v-slot:item.streaming="{ item }">
+              <v-icon v-if="item.streaming" color="black"
+                >mdi-access-point</v-icon
+              >
+            </template>
+
+            <template v-slot:item.recording="{ item }">
+              <v-icon v-if="item.recording" color="red">mdi-circle</v-icon>
+            </template>
+
+            <template v-slot:item.more="{ item }">
+              <v-icon :color="item.more">mdi-dots-horizontal</v-icon>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import "reflect-metadata";
 @Component
-export default class ParticipantsTable extends Vue {}
+export default class CasesTable extends Vue {
+  data() {
+    return {
+      headers: [
+        { text: "Active", value: "active", align: "center" },
+        { text: "Name", value: "name", align: "center" },
+        { text: "Case Number", value: "caseNumber", align: "center" },
+        { text: "Case Name", value: "caseName", align: "center" },
+        { text: "Role", value: "role", align: "center" },
+        { text: " ", value: "more", align: "center" },
+      ],
+      tableData: [
+        {
+          active: true,
+          name: "Danny Trejo",
+          caseNumber: "1234567890",
+          caseName: "Boris v. Natasha",
+          role: "Scheduled Participant (Dedicated)",
+          more: "",
+        },
+        {
+          active: true,
+          name: "Olivia Coleman",
+          caseNumber: "N/A",
+          caseName: "N/A",
+          role: "Participant (Shared)",
+          more: "",
+        },
+        {
+          active: false,
+          name: "Tom Hanks",
+          caseNumber: "3456789012",
+          caseName: "Up v. Down",
+          role: "Participant (Dedicated)",
+          more: "",
+        },
+      ],
+    };
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
