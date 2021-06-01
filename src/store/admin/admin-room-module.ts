c4c1a9069af1b7cead69f1aa5d4b73e4482b78b7
Paul Jerome <contactpauljerome@gmail.com>
2021-06-01 14:24:35 -0700

    Update admin-room-module to use Room interface

--- src/store/admin/admin-room-module.ts
+++ src/store/admin/admin-room-module.ts
@@ -1,5 +1,46 @@
 import { Module } from "vuex";
-import { Room } from "@/model/admin/room/room"
+import { Room } from "@/model/admin/room/room";
+
+const rooms: {
+  [key: string]: Room;
+} = {
+  a: {
+    uuid: "a",
+    roomSettings: {
+      template: "Template Name (Modified)",
+      passcode: "YourHonor931",
+      allowPasscode: true,
+      video: true,
+      audio: true,
+      allowSelectBeforeEnter: true,
+      waitingRoom: true,
+      providerPSTN: "ZIPDX",
+      allowParticipantsToggleVideo: true,
+      allowParticipantsToggleAudio: true,
+      defaultCanvasLayout: "Grid",
+      allowParticipantsChangeLayout: true,
+      videoQuality: "HIGH",
+      screenSharing: true,
+      requireScreenSharingRequest: true,
+      fileManagement: true,
+      handRaising: true,
+      chat: true,
+      recording: true,
+      emailLinkToAll: true,
+      transcription: true,
+      remoteTranslation: true,
+      streaming: true,
+      duplicateParticipantHandling: true,
+    },
+    roomDetails: {
+      organization: "LA County Superior Court - 12345",
+      status: "Available",
+      phone: "555-555-5555",
+      hostAccessCode: 5555,
+      participantAccessCode: 5555,
+    }
+  }
+}
 
 const roomSettings = {
   template: "Template Name (Modified)",
@@ -39,6 +80,7 @@ const roomDetails = {
 const adminRoomModule: Module<any, any> = {
   namespaced: true,
   state: {
+    rooms: rooms,
     roomSettings: roomSettings,
     roomDetails: roomDetails,
   },
