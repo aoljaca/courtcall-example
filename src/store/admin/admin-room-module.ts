1841040873d58bda415c7f7695cf1a5351eb0567
Paul Jerome <contactpauljerome@gmail.com>
2021-06-01 16:17:43 -0700

    Remove leftover dummy objects from before room interface was added

--- src/store/admin/admin-room-module.ts
+++ src/store/admin/admin-room-module.ts
@@ -42,48 +42,11 @@ const rooms: {
   }
 }
 
-const roomSettings = {
-  template: "Template Name (Modified)",
-  passcode: "YourHonor931",
-  allowPasscode: true,
-  video: true,
-  audio: true,
-  allowSelectBeforeEnter: true,
-  waitingRoom: true,
-  providerPSTN: "ZIPDX",
-  allowParticipantsToggleVideo: true,
-  allowParticipantsToggleAudio: true,
-  defaultCanvasLayout: "Grid",
-  allowParticipantsChangeLayout: true,
-  videoQuality: "HIGH",
-  screenSharing: true,
-  requireScreenSharingRequest: true,
-  fileManagement: true,
-  handRaising: true,
-  chat: true,
-  recording: true,
-  emailLinkToAll: true,
-  transcription: true,
-  remoteTranslation: true,
-  streaming: true,
-  duplicateParticipantHandling: true,
-}
-
-const roomDetails = {
-  organization: "LA County Superior Court - 12345",
-  status: "Available",
-  phone: "555-555-5555",
-  hostAccessCode: "5555",
-  participantAccessCode: "5555"
-}
-
 const adminRoomModule: Module<any, any> = {
   namespaced: true,
   state: {
     rooms: rooms,
     selectedRoomId: "a",
-    roomSettings: roomSettings,
-    roomDetails: roomDetails,
   },
   mutations: {
   },
