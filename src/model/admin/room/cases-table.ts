258b7530d247d974dfe9123c5f47fe1d7e09b284
Paul Jerome <contactpauljerome@gmail.com>
2021-06-01 16:36:13 -0700

    Add cases-table.ts and participants-table.ts to support room.ts interface

---
+++ src/model/admin/room/cases-table.ts
@@ -0,0 +1,6 @@
+export interface CasesTable {
+  status: boolean,
+  caseNumber: number,
+  caseName: string,
+  scheduledParticipants: string[],
+}
---
+++ src/model/admin/room/participants-table.ts
@@ -0,0 +1,7 @@
+export interface ParticipantsTable {
+  active: boolean,
+  name: string,
+  caseNumber: number,
+  caseName: string,
+  role: string,
+}
