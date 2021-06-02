76f276ff77b11c5a0820d4aecc2925871837d007
Paul Jerome <contactpauljerome@gmail.com>
2021-06-02 12:27:15 -0700

    merge branch master into feature/room-view-manage

--- src/plugins/i18n/en-us/admin.ts
+++ src/plugins/i18n/en-us/admin.ts
@@ -1,6 +1,5 @@
 import dashboard from "./admin/dashboard";
 import systemUser from "./admin/system-user";
-<<<<<<< HEAD
 import roomSettings from "./admin/room/roomSettings";
 import roomDetails from "./admin/room/roomDetails"
 
@@ -9,11 +8,4 @@ export default {
   systemUser,
   roomSettings,
   roomDetails,
-=======
-import organizations from "./admin/organizations";
-export default {
-  dashboard,
-  systemUser,
-  organizations,
->>>>>>> dc8c4fb640f38430c2ae6f79317108da27b15db9
 };
