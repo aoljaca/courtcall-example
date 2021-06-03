a78ed0d0f3d8f9f40faed2550cb3e5b691946c96
Paul Jerome <contactpauljerome@gmail.com>
2021-06-02 17:10:03 -0700

    Remove inline styles, place form in a v-col nested in a v-row

--- src/components/admin/my-account/MyAccount.vue
+++ src/components/admin/my-account/MyAccount.vue
@@ -12,85 +12,88 @@
         >
       </v-col>
     </v-row>
-    <v-form class="ml-16">
-      <v-row class="mt-14 mb-2">
-        <v-col cols="6">
-          <span class="font-weight-regular text-h5">Frank Voci</span>
-        </v-col>
-        <v-col cols="6">
-          <v-row class="justify-end mr-16">
-            <v-col cols="2">
-              <v-btn 
-              style="color: white; position: relative; left: 1.75rem;" 
-              color="grey lighten-1 rounded-0" 
-              depressed
-              >{{ $t("admin.systemUser.cancel") }}</v-btn
-            >
+    <v-row class="mt-8">
+      <v-col>
+        <v-form class="ml-8">
+          <v-row class="mb-2">
+            <v-col cols="6">
+              <span class="font-weight-regular text-h5">Frank Voci</span>
             </v-col>
-            <v-col cols="2">
-              <v-btn 
-              style="color: white" 
-              color="grey darken-4 rounded-0" 
-              depressed
-              >{{ $t("admin.systemUser.save") }}</v-btn
-              >
+            <v-col cols="6">
+              <v-row class="justify-end mr-16">
+                <v-col cols="2">
+                  <v-btn 
+                  class="mx-8"
+                  color="grey lighten-1 rounded-0 white--text" 
+                  depressed
+                  >{{ $t("admin.systemUser.cancel") }}</v-btn
+                >
+                </v-col>
+                <v-col cols="2">
+                  <v-btn 
+                  color="grey darken-4 rounded-0 white--text" 
+                  depressed
+                  >{{ $t("admin.systemUser.save") }}</v-btn
+                  >
+                </v-col>
+              </v-row>
             </v-col>
           </v-row>
-        </v-col>
-      </v-row>
-      <v-row dense>
-        <v-col cols="1">
-          <label>{{ $t("admin.systemUser.email") }}</label>
-        </v-col>
-        <v-col cols="3">
-          <v-text-field v-model="email" readonly dense>
-          </v-text-field>
-        </v-col>
-      </v-row>
-      <v-row dense>
-        <v-col cols="1">
-          <label>{{ $t("admin.systemUser.phone") }}</label>
-        </v-col>
-        <v-col cols="3">
-          <v-text-field v-model="phone" dense>
-          </v-text-field>
-        </v-col>
-      </v-row>
-      <v-row class="mt-4 mb-4">
-        <v-col>
-          <span 
-          class="font-weight-medium"
-          >{{ $t("admin.systemUser.changePassword") }}</span>
-        </v-col>
-      </v-row>
-      <v-row dense>
-        <v-col cols="1">
-          <label>{{ $t("admin.systemUser.password") }}</label>
-        </v-col>
-        <v-col cols="3">
-          <v-text-field type="password" dense>
-          </v-text-field>
-        </v-col>
-      </v-row>
-      <v-row dense>
-        <v-col cols="1">
-          <label>{{ $t("admin.systemUser.newPassword") }}</label>
-        </v-col>
-        <v-col cols="3">
-          <v-text-field type="password" dense>
-          </v-text-field>
-        </v-col>
-      </v-row>
-      <v-row dense>
-        <v-col cols="1">
-          <label>{{ $t("admin.systemUser.confirmPassword") }}</label>
-        </v-col>
-        <v-col cols="3">
-          <v-text-field type="password" dense>
-          </v-text-field>
-        </v-col>
-      </v-row>
-    </v-form>
+          <v-row dense>
+            <v-col cols="1">
+              <label>{{ $t("admin.systemUser.email") }}</label>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field v-model="email" readonly dense>
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row dense>
+            <v-col cols="1">
+              <label>{{ $t("admin.systemUser.phone") }}</label>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field v-model="phone" dense>
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="mt-4 mb-4">
+            <v-col>
+              <span 
+              class="font-weight-medium"
+              >{{ $t("admin.systemUser.changePassword") }}</span>
+            </v-col>
+          </v-row>
+          <v-row dense>
+            <v-col cols="1">
+              <label>{{ $t("admin.systemUser.password") }}</label>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field type="password" dense>
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row dense>
+            <v-col cols="1">
+              <label>{{ $t("admin.systemUser.newPassword") }}</label>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field type="password" dense>
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row dense>
+            <v-col cols="1">
+              <label>{{ $t("admin.systemUser.confirmPassword") }}</label>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field type="password" dense>
+              </v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
