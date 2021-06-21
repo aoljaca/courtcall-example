18859bb94a2b3edd064a6da52b6a4fe6cde8b67a
Michael Sila <michael.sila.jobs@gmail.com>
2021-06-21 10:13:14 -0600

    added cache invalidation

--- cicd/deploy-dev.sh
+++ cicd/deploy-dev.sh
@@ -9,4 +9,4 @@ npm run build:prod
 
 echo 'Upload'
 aws s3 sync --delete dist/ s3://www.dev.ccv3dev.com/
-aws s3 cp s3://www.dev.ccv3dev.com/index.html s3://www.dev.ccv3dev.com/index.html --metadata-directive REPLACE --cache-control max-age=0 --content-type text/html
+aws create-invalidation --distribution-id=E18U0IWTM9CD00 --paths=index.html
