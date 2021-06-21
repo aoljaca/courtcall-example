f547ce621bb8573acab6405deaeac67d6fb39d81
Michael Sila <michael.sila.jobs@gmail.com>
2021-06-21 10:26:21 -0600

    updating master

--- cicd/deploy-dev.sh
+++ cicd/deploy-dev.sh
@@ -9,4 +9,4 @@ npm run build:prod
 
 echo 'Upload'
 aws s3 sync --delete dist/ s3://www.dev.ccv3dev.com/
-aws create-invalidation --distribution-id=E18U0IWTM9CD00 --paths=index.html
+aws cloudfront create-invalidation --distribution-id=E18U0IWTM9CD00 --paths=/index.html
