32ef0a83b113d000423a7e30417e0f18b97dd26a
Michael Sila <michael.sila.jobs@gmail.com>
2021-07-01 09:46:09 -0600

    changes to trigger cicd

--- cicd/deploy-dev.sh
+++ cicd/deploy-dev.sh
@@ -7,7 +7,6 @@ npm ci
 echo 'Build'
 npm run build:dev
 
-
 echo 'Upload'
 aws s3 sync --delete dist/ s3://www.dev.ccv3dev.com/
 aws cloudfront create-invalidation --distribution-id=E18U0IWTM9CD00 --paths=/index.html
