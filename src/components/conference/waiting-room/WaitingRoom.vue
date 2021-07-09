a3e28360940be0b465945a3cc1e13afabb5be70f
Paul Jerome <contactpauljerome@gmail.com>
2021-07-08 17:08:49 -0700

    Change to v-alert, add translations

--- src/components/conference/waiting-room/WaitingRoom.vue
+++ src/components/conference/waiting-room/WaitingRoom.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="conference-body-height d-flex justify-space-around flex-column" id="waiting-room-container">
+  <div class="conference-body-height d-flex justify-space-around flex-column" id="waitingRoom-container">
     <v-row id="top-level-row" class="flex-grow-0">
       <v-col id="message-host" class="m-l-25 pb-4">
         <v-alert type="warning" icon="mdi-alert-circle-outline">
-          Cases are running 10-15 minutes behind schedule today. 
-          Please be prepared to wait until your case is called.
+          {{ $t("waitingRoom.alert")}}
         </v-alert>
         <v-row>
           <v-col>
             <h1>
-              Please wait here.
+              {{ $t("waitingRoom.mainHeading")}}
             </h1>
           </v-col>
         </v-row>
@@ -19,7 +18,7 @@
               The pressure cooker
             </h4>
             <div class="mx-4">
-              Your host will let you into the room soon
+              {{ $t("waitingRoom.subHeading")}}
             </div>
           </v-col>
         </v-row>
@@ -30,7 +29,7 @@
               depressed
               data-test-id="message-host-button"
             >
-              Message the host
+              {{ $t("waitingRoom.contactHost")}}
             </v-btn>
           </v-col>
           <v-col>
@@ -39,12 +38,12 @@
               depressed
               data-test-id="exit-button"
             >
-              Exit
+              {{ $t("waitingRoom.exit") }}
             </v-btn>
           </v-col>
         </v-row>
       </v-col>
-      <v-col id="waiting-room-content" class="m-r-25" align-self="end">
+      <v-col id="waitingRoom-content" class="m-r-25" align-self="end">
         <v-row id="watch">
           <v-col>
             <div class="d-flex">
@@ -52,23 +51,23 @@
                 mdi-play-circle-outline
               </v-icon>
               <h4 class="mx-2">
-                Watch
+                {{ $t("waitingRoom.watch")}}
               </h4>
             </div>
             <div id="brief-video">
-              Check out the 
+              {{ $t("waitingRoom.watchMsg1")}} 
               <a>
-              brief video
+              {{ $t("waitingRoom.watchLink")}}
               </a>
-              on the Los Angeles County Virtual Courthouse rules and procedures. 
             </div>
+            {{ $t("waitingRoom.watchMsg2")}}
           </v-col>
         </v-row>
         <v-row id="read">
           <v-col>
             <div class="d-flex">
               <h4>
-                Read
+                {{ $t("waitingRoom.read")}}
               </h4>
               <v-icon class="mx-2">
                 mdi-book
@@ -76,9 +75,9 @@
             </div>
             <div id="click-here">
               <a>
-              Click here 
+              {{ $t("waitingRoom.readLink") }}
               </a>
-              to view this document relating to today’s case before entering the courtroom. 
+              {{ $t("waitingRoom.readMsg") }}
             </div>
           </v-col>
         </v-row>
@@ -89,49 +88,8 @@
 <script lang="ts">
 import "reflect-metadata";
 import { Component, Vue } from "vue-property-decorator";
-import WaitingRoomNotification from "./WaitingRoomNotification.vue";
-import { IWaitingRoomNotification } from "@/model/meeting/waiting-room/waiting-room-notification";
-type ContactState = "uncontacted" | "contacting" | "contacted";
-@Component({
-  components: {
-    WaitingRoomNotification,
-  },
-})
-export default class WaitingRoom extends Vue {
-  hostMessage = "";
-  contactState: ContactState = "uncontacted";
-  waitingRoomNotifications: IWaitingRoomNotification[] = [
-    {
-      iconClass: "mdi-book",
-      linkDescription: "Read",
-      link: "http://wikipedia.org",
-      description: "This is an example of a read link",
-      color: "lightblue",
-    },
-    {
-      iconClass: "mdi-youtube",
-      linkDescription: "Watch",
-      link: "http://youtube.com",
-      description: "This is an example of a watch link",
-      color: "#fc979e",
-    },
-    {
-      iconClass: "mdi-information",
-      description: "This is an example of a general link",
-      color: "#ffd359",
-    },
-  ];
-
-  get validMessage() {
-    return this.hostMessage.length <= 300 && this.hostMessage.length > 0;
-  }
-  contactHost() {
-    this.contactState = "contacting";
-  }
-  sendMessage() {
-    this.contactState = "contacted";
-  }
-}
+@Component
+export default class WaitingRoom extends Vue {}
 </script>
 <style lang="scss" scoped>
 .big-space {
@@ -152,11 +110,5 @@ a {
 }
 .m-r-25 {
   margin-right: 25%;
-}
-.m-t-25 {
-  margin-top: 25%;
-}
-.m-b-25 {
-  margin-bottom: 25%;
 }
 </style>
--- src/plugins/i18n/en-us/waiting-room.ts
+++ src/plugins/i18n/en-us/waiting-room.ts
@@ -1,9 +1,18 @@
 export default {
-  mainHeading: "Please Wait Here",
+  mainHeading: "Please Wait Here.",
   subHeading: "Your host will let you into the room soon",
+  read: "Read",
+  readLink: "Click here",
+  readMsg: "to view this document relating to today’s case before entering the courtroom.",
+  watch: "Watch",
+  watchMsg1: "Check out the",
+  watchLink: "brief video",
+  watchMsg2: "on the Los Angeles County Virtual Courthouse rules and procedures.",
   notices: "Important Notices",
   contactHost: "Contact Host",
+  exit: "Exit",
   contactHostDescription: "Please leave a brief message for the host",
   hostContacted: "The host has been contacted. Please stand by",
   characterLimit: "Max 300 Characters",
+  alert: "Cases are running 10-15 minutes behind schedule today. Please be prepared to wait until your case is called."
 };
