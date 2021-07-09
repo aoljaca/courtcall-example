f027e060eeae9087db04886cfbe537f86214e940
Paul Jerome <contactpauljerome@gmail.com>
2021-07-09 10:48:28 -0700

    Make dialog its own component

---
+++ src/components/conference/waiting-room/ContactHost.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    width="700"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+        color="black--text"
+        depressed
+        data-test-id="contact-host-button"
+      >
+        {{ $t("waitingRoom.contactHost")}}
+      </v-btn>
+    </template>
+
+    <v-card class="pa-14">
+      <v-card-title class="text-h5 d-flex justify-center">
+        <div>
+          Message the host
+        </div>
+      </v-card-title>
+
+      <v-text-field
+        data-test-id="contact-host-text"
+        clearable
+        class="mb-4"
+      >
+      </v-text-field>
+
+      <v-card-actions class="d-flex justify-center">
+        <v-btn
+          depressed
+          data-test-id="contact-host-submit"
+          class="pa-6"
+          color="accent white--text"
+          @click="dialog = false"
+        >
+          Submit
+        </v-btn>
+        <v-btn
+          class="mx-8 pa-6"
+          depressed
+          data-test-id="contact-host-cancel"
+          color="info black--text"
+          @click="dialog = false"
+        >
+          Cancel
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script lang="ts">
+import "reflect-metadata";
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class ContactHost extends Vue {
+  dialog = false;
+}
+</script>
+<style lang="scss" scoped>
+a {
+  text-decoration: underline;
+}
+.m-l-25 {
+  margin-left: 25%;
+}
+.m-r-25 {
+  margin-right: 25%;
+}
+</style>
--- src/components/conference/waiting-room/WaitingRoom.vue
+++ src/components/conference/waiting-room/WaitingRoom.vue
@@ -22,57 +22,10 @@
             </div>
           </v-col>
         </v-row>
-        <v-row id="message-or-exit">
+        <v-row id="contact-or-exit">
           <v-col>
-            <v-dialog
-              v-model="dialog"
-              width="500"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  v-bind="attrs"
-                  v-on="on"
-                  color="black--text"
-                  depressed
-                  data-test-id="message-host-button"
-                >
-                  {{ $t("waitingRoom.contactHost")}}
-                </v-btn>
-              </template>
-
-              <v-card class="pa-4">
-                <v-card-title class="text-h5 d-flex justify-center">
-                  <div>
-                    Message the host
-                  </div>
-                </v-card-title>
-
-                <v-text-field
-                  data-test-id="message-host-text"
-                  clearable
-                >
-                </v-text-field>
-
-                <v-card-actions class="d-flex justify-center">
-                  <v-btn
-                    depressed
-                    data-test-id="message-host-submit"
-                    color="primary white--text"
-                    @click="dialog = false"
-                  >
-                    Submit
-                  </v-btn>
-                  <v-btn
-                    depressed
-                    data-test-id="message-host-cancel"
-                    color="info black--text"
-                    @click="dialog = false"
-                  >
-                    Cancel
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+            <contact-host>
+            </contact-host>
           </v-col>
           <v-col>
             <v-btn
@@ -130,22 +83,17 @@
 <script lang="ts">
 import "reflect-metadata";
 import { Component, Vue } from "vue-property-decorator";
-@Component
+import ContaactHost from "@/components/conference/waiting-room/ContaactHost.vue";
+@Component({
+  components: {
+    ContaactHost,
+  }, 
+})
 export default class WaitingRoom extends Vue {
   dialog = false;
 }
 </script>
 <style lang="scss" scoped>
-.big-space {
-  padding-bottom: 15vh;
-}
-.contact-text {
-  color: gray;
-  font-size: 1.5rem;
-}
-.warning-background {
-  background-color: yellow;
-}
 a {
   text-decoration: underline;
 }
