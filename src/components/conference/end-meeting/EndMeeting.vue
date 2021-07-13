dc086b2ecf3a0ab6a0f51756fe0cea6ffcc0f56c
aoljaca <aoljaca@courtcall.com>
2021-07-13 08:28:21 -0400

    commit

--- src/components/conference/end-meeting/EndMeeting.vue
+++ src/components/conference/end-meeting/EndMeeting.vue
@@ -15,20 +15,14 @@
                 </v-col>
               </v-row>
               <v-row>
-                <v-col class="d-flex justify-center">
+                <v-col class="d-flex button-flex justify-center">
                   <span class="subHeader">{{ $t("logout.subHeader") }}</span>
                 </v-col>
               </v-row>
-              <v-row>
-                <v-col class="d-flex justify-center">
-                  <button>Add 1</button>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col class="d-flex justify-center">
-                  <button>Add 1</button>
-                </v-col>
-              </v-row>
+              <v-row class="button-flex-wrap">
+                    <button>Add 1</button>
+                    <button>Add 1</button>
+              </v-row>          
             </v-container>
           </v-card>
         </v-col>
@@ -55,4 +49,22 @@ h1 {
 .v-sheet.v-card:not(.v-sheet--outlined) {
   box-shadow: 0 0 0 0 !important;
 }
+.button-flex {
+  display: flex;
+  width: 300px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 !important;
+}
+button {
+  background-color: #F5F5F5;
+  padding: 10px 20px
+}
+.button-flex-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 !important;
+}
+
 </style>
