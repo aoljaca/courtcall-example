2d746d431683efb48cc3abd2d5a29d6f690cd899
aoljaca <aoljaca@courtcall.com>
2021-07-13 13:43:43 -0400

    commit

--- src/components/conference/end-meeting/EndMeeting.vue
+++ src/components/conference/end-meeting/EndMeeting.vue
@@ -1,42 +1,37 @@
 <template>
-  <div id="endMeeting">
-    <v-container class="fill d-flex align-content-center">
-      <v-row class="justify-center">
-        <v-col align-self="center" cols="12" md="6">
-          <v-card class="border-white justify-center center-text-left">
-            <v-container fluid>
-              <v-container class="d-flex justify-start set-width">
-                <v-row>
-                  <v-col>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col class="d-flex">
-                    <h1 class="text-left">{{ $t("logout.heading") }}</h1>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col class="d-flex button-flex">
-                    <div class="subHeader">{{ $t("logout.subHeader") }}</div>
-                  </v-col>
-                </v-row>
-                <v-row class="d-flex buttons">
-                      <button class="button-1">Add 1</button>
-                      <button>Add 1</button>
-                </v-row>
-              </v-container>                  
-            </v-container>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-container class="fill d-flex align-content-center">
+    <v-row class="justify-center">
+      <v-col align-self="center" cols="auto">
+          <v-row>
+            <v-col>
+              <h1>{{ $t("logout.heading") }}</h1>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <div class="subHeader">{{ $t("logout.subHeader") }}</div>
+            </v-col>
+          </v-row>
+          <v-row class="buttons">
+            <v-btn @click="onReJoin">{{ $t("logout.rejoin") }}</v-btn>
+            <v-btn @click="onSupport">{{ $t("logout.support") }}</v-btn> 
+          </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import "reflect-metadata";
 @Component
-export default class EndMeeting extends Vue {}
+export default class EndMeeting extends Vue {
+  onReJoin(): void {
+    
+  }
+  onSupport(): void {
+    
+  }
+}
 </script>
 <style lang="scss" scoped>
 .fill {
@@ -48,48 +43,14 @@ h1 {
 .subHeader {
   margin-top: -15px;
 }
-.v-sheet.v-card:not(.v-sheet--outlined) {
-  box-shadow: 0 0 0 0 !important;
-}
-.button-flex {
-  width: 300px;
-  margin: 0 !important;
-}
-button {
-  background-color: #F5F5F5;
-  padding: 10px 20px;
-}
-.container-css {
-  width: 30vw;
-}
-.center-text-left {
-  text-align: left;
-}
-.set-width {
-  width: 500px;
-  flex-direction: column;
-  margin-right: -40px;
-}
-.button-1 {
-  margin-right: 25px; 
-}
 .buttons {
   margin-top: 30px !important;
   margin-left: 0px !important;
   margin-right: 0px !important; 
 }
-@media only screen and (max-width: 600px) {
-  .set-width {
-    width: 300px;
-    flex-direction: column;
-  }
-}
-
-@media only screen and (max-width: 590px) {
-  .set-width {
-    width: 300px;
-    flex-direction: column;
-    margin-right: 0;
-  }
+.v-btn--is-elevated {
+  box-shadow: 0 0 0 0;
+  background-color: #F5F5F5;
+  padding: 10px 20px;
 }
 </style>
--- src/plugins/i18n/en-us/logout.ts
+++ src/plugins/i18n/en-us/logout.ts
@@ -2,6 +2,6 @@ export default {
   heading: "Thanks for participating",
   subHeader:
     "For further information about your meeting please contact your host",
-  rejoin: "to rejoin your meeting",
-  support: "If you need assistance from CourtCall",
+  rejoin: "Rejoin your meeting",
+  support: "Request assistance",
 };
