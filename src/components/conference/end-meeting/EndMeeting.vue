24c5242f10bd28ee3e8aa6c6585ddccaa6849cb7
aoljaca <aoljaca@courtcall.com>
2021-07-13 09:28:37 -0400

    commit

--- src/components/conference/end-meeting/EndMeeting.vue
+++ src/components/conference/end-meeting/EndMeeting.vue
@@ -3,26 +3,28 @@
     <v-container class="fill d-flex align-content-center">
       <v-row class="justify-center">
         <v-col align-self="center" cols="12" md="6">
-          <v-card class="border-white">
+          <v-card class="border-white justify-center center-text-left">
             <v-container fluid>
-              <v-row>
-                <v-col>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <h1 class="text-center">{{ $t("logout.heading") }}</h1>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col class="d-flex button-flex justify-center">
-                  <span class="subHeader">{{ $t("logout.subHeader") }}</span>
-                </v-col>
-              </v-row>
-              <v-row class="button-flex-wrap">
-                    <button>Add 1</button>
-                    <button>Add 1</button>
-              </v-row>          
+              <v-container class="d-flex justify-start set-width">
+                <v-row>
+                  <v-col>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="d-flex">
+                    <h1 class="text-left">{{ $t("logout.heading") }}</h1>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="d-flex button-flex">
+                    <div class="subHeader">{{ $t("logout.subHeader") }}</div>
+                  </v-col>
+                </v-row>
+                <v-row class="d-flex button-flex-wrap">
+                      <button>Add 1</button>
+                      <button>Add 1</button>
+                </v-row>
+              </v-container>                  
             </v-container>
           </v-card>
         </v-col>
@@ -50,21 +52,24 @@ h1 {
   box-shadow: 0 0 0 0 !important;
 }
 .button-flex {
-  display: flex;
   width: 300px;
-  align-items: center;
-  justify-content: center;
   margin: 0 !important;
 }
 button {
   background-color: #F5F5F5;
-  padding: 10px 20px
+  padding: 10px 20px;
 }
 .button-flex-wrap {
-  display: flex;
-  align-items: center;
-  justify-content: center;
   margin: 0 !important;
 }
-
+.container-css {
+  width: 30vw;
+}
+.center-text-left {
+  text-align: left;
+}
+.set-width {
+  width: 300px;
+  flex-direction: column;
+}
 </style>
